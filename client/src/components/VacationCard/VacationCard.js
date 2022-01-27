c0fdb81f56f4fa094099b7f2ebe9bf01e8191473
nluuarch@gmail.com <nluuarch@gmail.com>
2022-01-26 22:43:25 -0800

    created add food modal with handle functions

--- client/src/components/VacationCard/VacationCard.js
+++ client/src/components/VacationCard/VacationCard.js
@@ -8,13 +8,15 @@ import "./vacationcard.scss"
 
 
 export default function VacationCard({body, vacation, handleDelete, handleClick, buttonText}) {
-  const [lodgingModalShow, setLodginglodgingModalShow] = React.useState(false);
+  const [lodgingModalShow, setLodgingModalShow] = React.useState(false);
   const [foodModalShow, setFoodModalShow] = React.useState(false);
   const [activityModalShow, setActivityModalShow] = React.useState(false);
 
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [lodging, setLodging] = useState(null)
+  const [foods, setFoods] = useState(null)
+
 
   let lodgingName
   let lodgingAddress
@@ -22,6 +24,13 @@ export default function VacationCard({body, vacation, handleDelete, handleClick,
   let check_in
   let check_out
   let lodgingEstimatedCost
+
+  let foodName
+  let foodAddress
+  let foodUrl
+  let foodHours
+  let foodDesc
+  let foodEstimatedCost
 
 
   function handleSubmitLodging(e) {
@@ -50,7 +59,6 @@ export default function VacationCard({body, vacation, handleDelete, handleClick,
         }
       });
   }
-
   function handleSetLodgingName(e){
     e.preventDefault();
     lodgingName = e.target.value
@@ -75,107 +83,240 @@ export default function VacationCard({body, vacation, handleDelete, handleClick,
     e.preventDefault();
     lodgingEstimatedCost = e.target.value
   }
+  function AddLodgingModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="m"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+            New Lodging Info:
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+                <Form onSubmit={handleSubmitLodging}>
+                    <div className="form-group">
+                        <label>Name*</label>
+                        <input 
+                            type="lodgingname" 
+                            className="form-control" 
+                            id="lodgingname-input" 
+                            placeholder="Enter name..." 
+                            onChange={handleSetLodgingName}
+                            autoComplete="off"
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Address*</label>
+                        <input 
+                            type="address" 
+                            className="form-control" 
+                            id="address-input" 
+                            placeholder="Enter address..."
+                            autoComplete="off"
+                            onChange={handleSetLodgingAddress}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Website*</label>
+                        <input 
+                            type="url" 
+                            className="form-control" 
+                            id="url-input" 
+                            placeholder="Enter Website..."
+                            autoComplete="off"
+                            onChange={handleSetLodgingUrl}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Check-In Time*</label>
+                        <input 
+                            type="checkin" 
+                            className="form-control" 
+                            id="checkin-input" 
+                            placeholder="00:00AM"
+                            autoComplete="off"
+                            onChange={handleSetLodgingCheckin}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Check-Out Time*</label>
+                        <input 
+                            type="checkin" 
+                            className="form-control" 
+                            id="checkin-input" 
+                            placeholder="00:00PM"
+                            autoComplete="off"
+                            onChange={handleSetLodgingCheckout}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Estimated Cost*</label>
+                        <input 
+                            type="cost" 
+                            className="form-control" 
+                            id="cost-input" 
+                            placeholder="$0"
+                            autoComplete="off"
+                            onChange={handleSetLodgingCost}
+                        ></input>
+                    </div>
+                    <div>
+                        <Button 
+                            style={{ backgroundColor: "#3E5C76", margin: "1%"}}
+                            type="submit"
+                        >{isLoading ? "Loading..." : "Submit"}
+                        </Button>
+                    </div>
+                    <div>
+                        {errors.map((err) => (
+                            <div key={err}>{err}</div>
+                        ))}
+                    </div>
+                </Form>
+        </Modal.Body>
+        </Modal>
+    );
+  }
 
-function AddLodgingModal(props) {
-  return (
-      <Modal
-          {...props}
-          size="m"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-      >
-      <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-          New Lodging Info:
-          </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-              <Form onSubmit={handleSubmitLodging}>
-                  <div className="form-group">
-                      <label>Name*</label>
-                      <input 
-                          type="lodgingname" 
-                          className="form-control" 
-                          id="lodgingname-input" 
-                          placeholder="Enter name..." 
-                          onChange={handleSetLodgingName}
-                          autoComplete="off"
-                      ></input>
-                  </div>
-                  <div className="form-group">
-                      <label>Address*</label>
-                      <input 
-                          type="address" 
-                          className="form-control" 
-                          id="address-input" 
-                          placeholder="Enter address..."
-                          autoComplete="off"
-                          onChange={handleSetLodgingAddress}
-                      ></input>
-                  </div>
-                  <div className="form-group">
-                      <label>Website*</label>
-                      <input 
-                          type="url" 
-                          className="form-control" 
-                          id="url-input" 
-                          placeholder="Enter Website..."
-                          autoComplete="off"
-                          onChange={handleSetLodgingUrl}
-                      ></input>
-                  </div>
-                  <div className="form-group">
-                      <label>Check-In Time*</label>
-                      <input 
-                          type="checkin" 
-                          className="form-control" 
-                          id="checkin-input" 
-                          placeholder="00:00AM"
-                          autoComplete="off"
-                          onChange={handleSetLodgingCheckin}
-                      ></input>
-                  </div>
-                  <div className="form-group">
-                      <label>Check-Out Time*</label>
-                      <input 
-                          type="checkin" 
-                          className="form-control" 
-                          id="checkin-input" 
-                          placeholder="00:00PM"
-                          autoComplete="off"
-                          onChange={handleSetLodgingCheckout}
-                      ></input>
-                  </div>
-                  <div className="form-group">
-                      <label>Estimated Cost*</label>
-                      <input 
-                          type="cost" 
-                          className="form-control" 
-                          id="cost-input" 
-                          placeholder="$0"
-                          autoComplete="off"
-                          onChange={handleSetLodgingCost}
-                      ></input>
-                  </div>
-                  <div>
-                      <Button 
-                          style={{ backgroundColor: "#3E5C76", margin: "1%"}}
-                          type="submit"
-                      >{isLoading ? "Loading..." : "Submit"}
-                      </Button>
-                  </div>
-                  <div>
-                      {errors.map((err) => (
-                          <div key={err}>{err}</div>
-                      ))}
-                  </div>
-              </Form>
-      </Modal.Body>
-      </Modal>
-  );
-}
+
+  function handleSubmitFood(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/foods", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+        name: foodName,
+        address: foodAddress,
+        url: foodUrl,
+        hours: foodHours,
+        desc: foodDesc,
+        estimated_cost: foodEstimatedCost,
+        likes: 0
+        }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((foods) => setFoods(foods));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+  function handleSetFoodName(e){
+    e.preventDefault();
+    foodName = e.target.value
+  }
+  function handleSetFoodAddress(e){
+    e.preventDefault();
+    foodAddress = e.target.value
+  }
+  function handleSetFoodUrl(e){
+    e.preventDefault();
+    foodUrl = e.target.value
+  }
+  function handleSetFoodHours(e){
+    e.preventDefault();
+    foodHours = e.target.value
+  }
+  function handleSetFoodCost(e){
+    e.preventDefault();
+    handleSetFoodCost = e.target.value
+  }
+  function AddFoodModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="m"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+            New Food Info:
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+                <Form onSubmit={handleSubmitFood}>
+                    <div className="form-group">
+                        <label>Name*</label>
+                        <input 
+                            type="foodname" 
+                            className="form-control" 
+                            id="foodname-input" 
+                            placeholder="Enter name..." 
+                            onChange={handleSetFoodName}
+                            autoComplete="off"
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Address*</label>
+                        <input 
+                            type="address" 
+                            className="form-control" 
+                            id="address-input" 
+                            placeholder="Enter address..."
+                            autoComplete="off"
+                            onChange={handleSetFoodAddress}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Website*</label>
+                        <input 
+                            type="url" 
+                            className="form-control" 
+                            id="url-input" 
+                            placeholder="Enter Website..."
+                            autoComplete="off"
+                            onChange={handleSetFoodUrl}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Check-In Time*</label>
+                        <input 
+                            type="foodhours" 
+                            className="form-control" 
+                            id="foodhours-input" 
+                            placeholder="00:00AM - 00:00PM"
+                            autoComplete="off"
+                            onChange={handleSetFoodHours}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Estimated Cost*</label>
+                        <input 
+                            type="cost" 
+                            className="form-control" 
+                            id="cost-input" 
+                            placeholder="$0"
+                            autoComplete="off"
+                            onChange={handleSetFoodCost}
+                        ></input>
+                    </div>
+                    <div>
+                        <Button 
+                            style={{ backgroundColor: "#3E5C76", margin: "1%"}}
+                            type="submit"
+                        >{isLoading ? "Loading..." : "Submit"}
+                        </Button>
+                    </div>
+                    <div>
+                        {errors.map((err) => (
+                            <div key={err}>{err}</div>
+                        ))}
+                    </div>
+                </Form>
+        </Modal.Body>
+        </Modal>
+    );
+  }
   
-  console.log(vacation)
   return (
     <nav className="vacation-container">
           {body === "deleted" ? 
@@ -229,20 +370,24 @@ function AddLodgingModal(props) {
                   <Button 
                     className="button" 
                     style={{ backgroundColor: "#3E5C76", margin: "1%"}}
-                    onClick={() => setLodginglodgingModalShow(true)} 
+                    onClick={() => setLodgingModalShow(true)} 
                   >Add Lodging</Button>
                   <AddLodgingModal
                     show={lodgingModalShow}
-                    onHide={() => setLodginglodgingModalShow(false)}
+                    onHide={() => setLodgingModalShow(false)}
                   />
                   </th> : null}
                 {vacation.foods && body === "card" ? <th scope="col">
                   <h5>Where To Eat</h5>
                   <Button 
-                        onClick={foodModalShow} 
                         className="button" 
                         style={{ backgroundColor: "#3E5C76", margin: "1%"}}
+                        onClick={() => setFoodModalShow(true)} 
                   >Add Food</Button>
+                  <AddFoodModal
+                    show={foodModalShow}
+                    onHide={() => setFoodModalShow(false)}
+                  />
                 </th> : null}
                 {vacation.activities && body === "card" ? <th scope="col">
                   <h5>What to do</h5>
